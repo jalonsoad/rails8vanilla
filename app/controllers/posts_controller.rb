5b7b99ac4e65838e6d1a6f6e1ff9323298cc95de
Julio Alonso <juliocesar.alonso@gmail.com>
2024-11-19 17:15:44 +0000

    New update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
 
   # GET /posts or /posts.json
   def index
-    #@posts = Post.all
+    # @posts = Post.all
     @posts = Post.with_rich_text_body.order(created_at: :desc)
   end
 
